1b88ff8cc1303285e94269721199e08442e5fd11
Rhomadon <adonrhomadon100@gmail.com>
2022-02-01 18:24:02 +0700

    Membuat table dinamis

--- r-library.js
+++ r-library.js
@@ -1,3 +1,23 @@
-const sectiontable = document.querySelector("section#table");
-sectiontable.innerHTML =
-  '<table class="table"><thead><tr><th scope="col">#</th><th scope="col">First</th><th scope="col">Last</th><th scope="col">Handle</th></tr></thead><tbody><tr><th scope="row">1</th><td>Mark</td><td>Otto</td><td>@mdo</td></tr><tr><th scope="row">2</th><td>Jacob</td><td>Thornton</td><td>@fat</td></tr><tr><th scope="row">3</th><td colspan="2">Larry the Bird</td><td>@twitter</td></tr></tbody></table>';
+let personData = [
+  { nama: "Rhomadon", jurusan: "Teknik Informatika", lulus: "2020" },
+  { nama: "Miqdad", jurusan: "Teknik Elektro", lulus: "2019" },
+  { nama: "Azam", jurusan: "Teknik Industri", lulus: "2021" },
+  { nama: "Hanif", jurusan: "Teknik Sipil", lulus: "2020" },
+];
+
+window.onload = () => {
+  loadTableData(personData);
+};
+
+function loadTableData(personData) {
+  const tableBody = document.getElementById("tableData");
+  let dataHTML = "";
+  let i = 1;
+
+  for (let person of personData) {
+    dataHTML += "<tr><th>" + i + "</th><td>" + person.nama + "</td><td>" + person.jurusan + "</td><td>" + person.lulus + "</td></tr>";
+    i++;
+  }
+
+  tableBody.innerHTML = dataHTML;
+}
