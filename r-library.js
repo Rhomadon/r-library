171c361403f3629f06514d17e45ccb64d2a8326b
Rhomadon <adonrhomadon100@gmail.com>
2022-02-02 10:40:32 +0700

    Mengubah Table Dinamis

--- r-library.js
+++ r-library.js
@@ -1,23 +1,51 @@
-let personData = [
-  { nama: "Rhomadon", jurusan: "Teknik Informatika", lulus: "2020" },
-  { nama: "Miqdad", jurusan: "Teknik Elektro", lulus: "2019" },
-  { nama: "Azam", jurusan: "Teknik Industri", lulus: "2021" },
-  { nama: "Hanif", jurusan: "Teknik Sipil", lulus: "2020" },
-];
-
-window.onload = () => {
-  loadTableData(personData);
-};
-
-function loadTableData(personData) {
-  const tableBody = document.getElementById("tableData");
-  let dataHTML = "";
-  let i = 1;
-
-  for (let person of personData) {
-    dataHTML += "<tr><th>" + i + "</th><td>" + person.nama + "</td><td>" + person.jurusan + "</td><td>" + person.lulus + "</td></tr>";
-    i++;
+class Table {
+  constructor(table) {
+    this.table = table;
   }
 
-  tableBody.innerHTML = dataHTML;
+  createHeader(data) {
+    let open = "<thead><tr>";
+    let close = "</tr></thead>";
+    data.forEach((d) => {
+      open += `<th>${d}</th>`;
+    });
+
+    return open + close;
+  }
+
+  createBody(data) {
+    let open = "<tbody>";
+    let close = "</tbody>";
+
+    data.forEach((d) => {
+      open += `
+      <tr>
+        <td>${d[0]}</td>
+        <td>${d[1]}</td>
+        <td>${d[2]}</td>
+        <td>${d[3]}</td>
+      </tr>
+    `;
+    });
+
+    return open + close;
+  }
+
+  render(element) {
+    let table = "<table class='table table-success table-striped'>" + this.createHeader(this.table.columns) + this.createBody(this.table.data) + "</table>";
+    element.innerHTML = table;
+  }
 }
+
+const table = new Table({
+  columns: ["No", "Name", "Jurusan", "Lulus"],
+  data: [
+    ["1", "Rhomadon", "Teknik Informatika", "2020"],
+    ["2", "Miqdad", "Teknik Elektro", "2019"],
+    ["3", "Azam", "Teknik Industri", "2018"],
+    ["4", "Hanif", "Teknik Sipil", "2020"],
+  ],
+});
+
+const app = document.getElementById("tableData");
+table.render(app);
