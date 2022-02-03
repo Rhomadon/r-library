8aa8b7eca28aac33f9915687fe8f3293511a9e1b
Rhomadon <adonrhomadon100@gmail.com>
2022-02-03 16:37:53 +0700

    Membuat Modul Revisi

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-import appTable from "./module.js";
+import Table from "./module.js";
 
-console.log(appTable());
+new Table({
+  columns: ["No", "Nama", "Jurusan", "Lulus"],
+  data: [
+    ["1", "Rhomadon", "Teknik Informatika", "2020"],
+    ["2", "Miqdad", "Teknik Elektro", "2019"],
+    ["3", "Azam", "Teknik Industri", "2018"],
+    ["4", "Hanif", "Teknik Sipil", "2020"],
+  ],
+}).render(document.getElementById("tableData1"));
+
+new Table({
+  columns: ["No", "Nama", "Pembimbing", "Keterangan"],
+  data: [
+    ["1", "Shinta", "Teknik Informatika", "-"],
+    ["2", "Siti", "Teknik Elektro", "-"],
+    ["3", "Ulum", "Teknik Industri", "-"],
+    ["4", "Randy", "Teknik Sipil", "-"],
+  ],
+}).render(document.getElementById("tableData2"));
--- module.js
+++ module.js
@@ -1,25 +1,24 @@
-export default function appTable() {
-  class Table {
-    constructor(table) {
-      this.table = table;
-    }
+export default class Table {
+  constructor(table) {
+    this.table = table;
+  }
 
-    createHeader(data) {
-      let open = "<thead><tr>";
-      let close = "</tr></thead>";
-      data.forEach((d) => {
-        open += `<th>${d}</th>`;
-      });
+  createHeader(data) {
+    let open = "<thead><tr>";
+    let close = "</tr></thead>";
+    data.forEach((d) => {
+      open += `<th>${d}</th>`;
+    });
 
-      return open + close;
-    }
+    return open + close;
+  }
 
-    createBody(data) {
-      let open = "<tbody>";
-      let close = "</tbody>";
+  createBody(data) {
+    let open = "<tbody>";
+    let close = "</tbody>";
 
-      data.forEach((d) => {
-        open += `
+    data.forEach((d) => {
+      open += `
         <tr>
           <td>${d[0]}</td>
           <td>${d[1]}</td>
@@ -27,29 +26,13 @@ export default function appTable() {
           <td>${d[3]}</td>
         </tr>
       `;
-      });
+    });
 
-      return open + close;
-    }
-
-    render(element) {
-      let table = "<table class='table table-success table-striped'>" + this.createHeader(this.table.columns) + this.createBody(this.table.data) + "</table>";
-      element.innerHTML = table;
-    }
+    return open + close;
   }
 
-  const table = new Table({
-    columns: ["No", "Name", "Jurusan", "Lulus"],
-    data: [
-      ["1", "Rhomadon", "Teknik Informatika", "2020"],
-      ["2", "Miqdad", "Teknik Elektro", "2019"],
-      ["3", "Azam", "Teknik Industri", "2018"],
-      ["4", "Hanif", "Teknik Sipil", "2020"],
-    ],
-  });
-
-  const appTable = document.getElementById("tableData");
-  table.render(appTable);
-
-  return appTable;
+  render(element) {
+    let table = "<table class='table table-success table-striped'>" + this.createHeader(this.table.columns) + this.createBody(this.table.data) + "</table>";
+    element.innerHTML = table;
+  }
 }
